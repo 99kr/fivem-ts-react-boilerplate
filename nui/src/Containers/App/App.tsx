a3061129ab67ab225a9d494a9b08e9081a747c88
Sebastian <43248481+99kr@users.noreply.github.com>
2020-01-21 19:10:11 +0100

    unused import

--- nui/src/Containers/App/App.tsx
+++ nui/src/Containers/App/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { FalseyValue } from 'styled-components';
+import styled from 'styled-components';
 
 import { useSelector } from '../../index';
 import Nui from "../../Nui";
