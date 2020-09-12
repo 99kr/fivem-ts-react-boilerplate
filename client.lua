5634feeecc2bf430746284a015f30695c4f25ab8
Sebastian <43248481+99kr@users.noreply.github.com>
2020-09-12 14:04:10 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -4,11 +4,3 @@ RegisterCommand("toggle", function()
     nuiOpen = not nuiOpen
     SendNUIMessage({type = "SHOW", payload = nuiOpen})
 end)
-
-Citizen.CreateThread(function()
-    Citizen.Wait(500)
-    SendNUIMessage({
-        type = "SEND_RESOURCENAME",
-        payload = GetCurrentResourceName()
-    })
-end)
